5365692f18e4e17f85156ca2ca11b3d0b385ba52
Isadora Ariane <65814808+sdrariane@users.noreply.github.com>
2022-09-15 19:39:57 -0300

    Add files via upload

---
+++ Rollercoasters/main.js
@@ -0,0 +1,29 @@
+let input = 691;
+
+function rollercoaster(input) {
+
+ // gonna convert the number to string
+ // and then split it into an array
+ let bob = input.toString().split('');
+ 
+ // we can't sort the array because of the
+ // 'feedbacks' of each rollercoaster
+ for(let i = 0; i < bobSorted.length; i++){
+  
+  // let's analyze the most 2 highest numbers
+  // they gonna tell us how scarier the rollercoasters
+  // are
+  if(bobSorted[i] >= bobSorted[i+1]){
+   console.log('scarier!');
+  }
+
+  // then we gonna see how far they are and set how much
+  // points bob can spend into it
+
+ }
+ 
+ return bob; 
+ 
+}
+
+console.log(bob);
